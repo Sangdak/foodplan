c06894efaa4750681cb5d7b0af68b08dd516bde7
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-09-20 02:44:58 +1000

    Added staticfiles dir and update settings.

--- foodplan/foodplan/settings.py
+++ foodplan/foodplan/settings.py
@@ -142,6 +142,10 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
