fbd97a8d0cb017137039819ef10a8814d3f5b027
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-09-22 14:51:32 +1000

    Added 'admin.py'.

---
+++ foodplan/foodplan/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+
+from .models import Culinary, Allergen, Category, Ingredient, Food, ImageFood
+
+
+@admin.register(Culinary)
+class CulinaryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+
+
+@admin.register(Allergen)
+class AllergenAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'image',
+    ]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'allergen',
+        'category',
+        'price',
+        'image',
+        'caloric',
+    ]
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'culinary',
+        'recipe',
+    ]
+
+
+@admin.register(ImageFood)
+class ImageFoodAdmin(admin.ModelAdmin):
+    list_display = [
+        'image',
+        'food',
+    ]
